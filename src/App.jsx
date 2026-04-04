542e8e5b81b53f4e317378f2b10e79142bf85eb9
Nayan Mondal <n007kingkong@gmail.com>
2026-04-04 21:25:53 +0600

    api problem fixed

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   const askQuestion = async ()=>{
 
     const response = await ai.models.generateContent({
-    model: "gemini-1.5-flash",
+    model: "gemini-2.5-flash-lite",
     contents: question,
   });
   setResult(response.text);
