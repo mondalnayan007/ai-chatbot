d547fba04ced3dd9ed6cc6f4ac811cab7de31cfc
Nayan Mondal <n007kingkong@gmail.com>
2026-04-04 18:40:33 +0600

    designed frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import React from "react"
 
 function App() {
-  
+
+  const asking = ()=>{
+
+    
+     
+  }
 
   return (
-    <>
-      <h2 className="">hello</h2>
-    </>
+    < div className="grid grid-cols-5 text-white text-center">
+        <div className="bg-[#1e2047] h-screen">
+             hello
+        </div>
+        <div className="col-span-4">
+          <h1>Hello user, Ask Anything to me....!!!!!!!!</h1>
+
+          <div className="h-[80%]">
+
+          </div>
+          <div className=" w-[60%] m-auto  bg-[#1e2047]  rounded-2xl border-2 border-zinc-600   flex px-4 py-3">
+             
+             <input type="search" name="" id="search" className="w-full h-full outline-none" placeholder="Ask Anything......." />
+             <button onClick={asking} className="cursor-pointer">Ask</button>
+            
+          </div>
+        
+        </div>
+    </div>
   )
 }
 
